7535490c36da112ade359c6486571886df36d538
Jiahao1995 <lijiahao5269@outlook.com>
2021-08-26 18:24:34 -0400

    implement friendships model and unit tests

--- honker/urls.py
+++ honker/urls.py
@@ -18,12 +18,14 @@ from django.urls import include, path
 from rest_framework import routers
 
 from accounts.api.views import UserViewSet, AccountViewSet
+from friendships.api.views import FriendshipViewSet
 from honks.api.views import HonkViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
 router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/honks', HonkViewSet, basename='honks')
+router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
